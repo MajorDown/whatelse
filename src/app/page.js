6ae8479ba0b4cd066aca54f7b94a4f514e56ed9b
MajorDown <romain.fouillaron@gmx.fr>
2023-07-18 23:56:10 +0200

    premiere version sous next.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,210 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [whatelseProjects, setWhatelseProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState();
+  const [projectName, setProjectName] = useState("");
+  const [newTask, setNewTask] = useState({
+    item: "",
+    color: "green",
+  });
+
+  useEffect(() => {
+    const storedProjects = localStorage.getItem("whatelseProjects");
+    if (storedProjects) {
+      const parsedProjects = JSON.parse(storedProjects);
+      setWhatelseProjects(parsedProjects);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("whatelseProjects", JSON.stringify(whatelseProjects));
+    console.log(JSON.parse(localStorage.getItem("whatelseProjects")));
+  }, [whatelseProjects]);
+
+  const handleChangeProjectName = (event) => {
+    setProjectName(event.target.value);
+  };
+
+  const handleCreateProject = () => {
+    if (projectName.trim() !== "") {
+      const newProject = {
+        name: projectName,
+        list: [],
+      };
+      setWhatelseProjects([...whatelseProjects, newProject]);
+      setProjectName("");
+    }
+  };
+
+  const handleChangeSelectedProject = (e, project) => {
+    setSelectedProject(project);
+  };
+
+  const handleDeleteProject = () => {
+    if (selectedProject) {
+      const updatedProjects = whatelseProjects.filter(
+        (project) => project !== selectedProject
+      );
+      setWhatelseProjects(updatedProjects);
+      setSelectedProject(null);
+    }
+  };
+
+  const handleCheck = (event, task) => {
+    const updatedTask = { ...task, done: event.target.checked };
+    const updatedProjectList = selectedProject.list.map((item) =>
+      item === task ? updatedTask : item
+    );
+    const updatedProject = { ...selectedProject, list: updatedProjectList };
+    setSelectedProject(updatedProject);
+    setWhatelseProjects((prevProjects) =>
+      prevProjects.map((project) =>
+        project === selectedProject ? updatedProject : project
+      )
+    );
+  };
+
+  const handleChangeNewItem = (event) => {
+    setNewTask({ ...newTask, [event.target.name]: event.target.value });
+  };
+
+  const handleAddTask = (event) => {
+    event.preventDefault();
+    if (selectedProject && newTask.item.trim() !== "") {
+      const updatedProjectList = [
+        ...selectedProject.list,
+        { ...newTask, done: false },
+      ];
+      const updatedProject = {
+        ...selectedProject,
+        list: updatedProjectList,
+      };
+      setSelectedProject(updatedProject);
+      setWhatelseProjects((prevProjects) =>
+        prevProjects.map((project) =>
+          project === selectedProject ? updatedProject : project
+        )
+      );
+      setNewTask({ item: "", color: "red" });
+    }
+  };
+
+  const handleDeleteItem = (e, task) => {
+    const updatedTaskList = selectedProject.list.filter(
+      (item) => item !== task
+    );
+    const updatedProject = { ...selectedProject, list: updatedTaskList };
+    setSelectedProject(updatedProject);
+    setWhatelseProjects((prevProjects) =>
+      prevProjects.map((project) =>
+        project === selectedProject ? updatedProject : project
+      )
+    );
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <>
+      <header>
+        <h1>Whatelse</h1>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Une appli pour se souvenir des trucs que tu oublies parce que t'as
+          d'autre trucs à te souvenirs... mais que tu te souviens plus... <br />
+          Bref. C'est une todolist.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+      </header>
+      <main>
+        <section id="projects">
+          <h2>Sur quel projet bossez-vous aujourd'hui ?</h2>
+          {whatelseProjects.map((project) => (
+            <button
+              key={project.name}
+              onClick={(e) => handleChangeSelectedProject(e, project)}
+            >
+              {project.name}
+            </button>
+          ))}
+          <div>
+            <p className="guideLine">
+              Un nouveau projet en tête ? donnez-lui un nom :
+            </p>
+            <input
+              type="text"
+              name="projectName"
+              id="projectName"
+              value={projectName}
+              onChange={(e) => {
+                handleChangeProjectName(e);
+              }}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <button
+              onClick={() => {
+                handleCreateProject();
+              }}
+            >
+              Créer
+            </button>
+          </div>
+        </section>
+        <section id="tasks">
+          {selectedProject && (
+            <>
+              <h2>
+                Projet : "{selectedProject.name}"
+                <button className="suppressorBtn" onClick={handleDeleteProject}>
+                  X
+                </button>
+              </h2>
+              <form onSubmit={handleAddTask}>
+                <p className="guideLine">ajouter un item :</p>
+                <input
+                  type="text"
+                  name="item"
+                  value={newTask.item}
+                  onChange={handleChangeNewItem}
+                  placeholder="Nom de l'item"
+                  required
+                />
+                <select
+                  name="color"
+                  value={newTask.color}
+                  onChange={handleChangeNewItem}
+                >
+                  <option value="red">Rouge</option>
+                  <option value="green">Vert</option>
+                  <option value="blue">Bleu</option>
+                  <option value="yellow">Jaune</option>
+                </select>
+                <button type="submit">Ajouter</button>
+              </form>
+              <ul>
+                {selectedProject.list.map((task) => (
+                  <li key={task.item}>
+                    <p>{task.item}</p>
+                    <div>
+                      <input
+                        type="checkbox"
+                        name="done"
+                        onChange={(e) => handleCheck(e, task)}
+                        checked={task.done}
+                      />
+                      <p>ok</p>
+                    </div>
+                    <button
+                      className="suppressorBtn"
+                      onClick={(e) => handleDeleteItem(e, task)}
+                    >
+                      X
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </section>
+      </main>
+      <footer>footer</footer>
+    </>
+  );
 }
